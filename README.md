64d0664362b4ed5635b92d5d176d3cdeaa4dd29a
Artem-Podoliak <podoliak.a.s@gmail.com>
2023-07-28 18:30:25 +0300

    well done

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# nodejs
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+node index.js --action list
+https://monosnap.com/file/Jnbi06vGUfjBAgB0qzwhyOeIJJxOQF
+
+# Получаем контакт по id - выводим в консоль объект контакта или null, если контакта с таким id не существует.
+
+node index.js --action get --id 05olLMgyVQdWRwgKfg5J6
+https://monosnap.com/file/19TSOZwRGlThUjxVH6aLs9SeFejHpu
+
+# Добавляем контакт и выводим в консоль созданный контакт
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+https://monosnap.com/file/K4gWA5RYoHXXNzimveSEnxg2toTtnp
+
+# Удаляем контакт и выводим в консоль удаленный контакт или null, если контакта с таким id не существует.
+
+node index.js --action remove --id qdggE76Jtbfd9eWJHrssH
+https://monosnap.com/file/BeM5y5QfqPgAZGOqxdrb71BcdPJlAo
---
+++ index.js
@@ -0,0 +1,41 @@
+const contact = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const all = await contact.listContacts();
+      console.log(all);
+      break;
+
+    case "get":
+      const oneContact = await contact.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const addContact = await contact.addContact(name, email, phone);
+      console.log(addContact);
+      break;
+
+    case "remove":
+      const removeContact = await contact.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+invokeAction(argv);
